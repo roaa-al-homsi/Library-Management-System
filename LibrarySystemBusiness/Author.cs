e8adfe6952c57e42b7d78f20f3a7e7cbe91b67ec
roaa-al-homsi <roaakokalhomsi@gmail.com>
2024-10-13 23:39:37 +0300

    Edit public Author Constructor change value member)

--- LibrarySystemBusiness/Author.cs
+++ LibrarySystemBusiness/Author.cs
@@ -18,7 +18,7 @@ namespace LibrarySystemBusiness
         {
             Person Person = new Person();
             this.Id = -1;
-            this.PersonId = PersonId;
+            this.PersonId = -1;
             this.Certificate = string.Empty;
             this.AdditionalDetails = string.Empty;
             _Mode = ModeAuthor.Add;
