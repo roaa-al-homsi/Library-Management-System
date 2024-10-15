8f75fa35f6bf6ff66e4c9c71eb5ff593c8d75bfe
roaa-al-homsi <roaakokalhomsi@gmail.com>
2024-10-15 19:47:48 +0300

    Adding Main Menu Screen To Desktou UI

---
+++ LibrarySystemDesktopUI/MainMenuScreen.Designer.cs
@@ -0,0 +1,300 @@
+﻿namespace LibraryStstem
+{
+    partial class MainMenuScreen
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainMenuScreen));
+            this.btnTitle = new Guna.UI2.WinForms.Guna2Button();
+            this.btnLogout = new Guna.UI2.WinForms.Guna2Button();
+            this.labUserName = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            this.guna2HtmlLabel1 = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            this.btnFindClient = new Guna.UI2.WinForms.Guna2Button();
+            this.btnShowClients = new Guna.UI2.WinForms.Guna2Button();
+            this.btnAddNew = new Guna.UI2.WinForms.Guna2Button();
+            this.guna2Panel2 = new Guna.UI2.WinForms.Guna2Panel();
+            this.btnUpdateClient = new Guna.UI2.WinForms.Guna2Button();
+            this.guna2Panel1 = new Guna.UI2.WinForms.Guna2Panel();
+            this.guna2HtmlLabel5 = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            this.guna2HtmlLabel3 = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            this.pnlOptions = new Guna.UI2.WinForms.Guna2Panel();
+            this.panelMainMenu = new Guna.UI2.WinForms.Guna2Panel();
+            this.guna2Panel2.SuspendLayout();
+            this.guna2Panel1.SuspendLayout();
+            this.pnlOptions.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnTitle
+            // 
+            this.btnTitle.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.btnTitle.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.btnTitle.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btnTitle.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.btnTitle.FillColor = System.Drawing.Color.Transparent;
+            this.btnTitle.Font = new System.Drawing.Font("Andalus", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTitle.ForeColor = System.Drawing.Color.White;
+            this.btnTitle.Image = ((System.Drawing.Image)(resources.GetObject("btnTitle.Image")));
+            this.btnTitle.ImageAlign = System.Windows.Forms.HorizontalAlignment.Left;
+            this.btnTitle.ImageSize = new System.Drawing.Size(40, 40);
+            this.btnTitle.Location = new System.Drawing.Point(3, 36);
+            this.btnTitle.Name = "btnTitle";
+            this.btnTitle.Size = new System.Drawing.Size(180, 45);
+            this.btnTitle.TabIndex = 2;
+            this.btnTitle.Text = "Home";
+            this.btnTitle.TextAlign = System.Windows.Forms.HorizontalAlignment.Left;
+            // 
+            // btnLogout
+            // 
+            this.btnLogout.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.btnLogout.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.btnLogout.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btnLogout.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.btnLogout.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnLogout.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(2)))), ((int)(((byte)(21)))), ((int)(((byte)(38)))));
+            this.btnLogout.Font = new System.Drawing.Font("Andalus", 10.8F);
+            this.btnLogout.ForeColor = System.Drawing.Color.White;
+            this.btnLogout.ImageAlign = System.Windows.Forms.HorizontalAlignment.Left;
+            this.btnLogout.ImageSize = new System.Drawing.Size(40, 40);
+            this.btnLogout.Location = new System.Drawing.Point(0, 373);
+            this.btnLogout.Name = "btnLogout";
+            this.btnLogout.Size = new System.Drawing.Size(210, 51);
+            this.btnLogout.TabIndex = 8;
+            this.btnLogout.Tag = "Logout";
+            this.btnLogout.Text = "Logout ";
+            // 
+            // labUserName
+            // 
+            this.labUserName.BackColor = System.Drawing.Color.Transparent;
+            this.labUserName.Font = new System.Drawing.Font("Andalus", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labUserName.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(173)))), ((int)(((byte)(73)))), ((int)(((byte)(225)))));
+            this.labUserName.Location = new System.Drawing.Point(827, 38);
+            this.labUserName.Name = "labUserName";
+            this.labUserName.Size = new System.Drawing.Size(3, 2);
+            this.labUserName.TabIndex = 1;
+            this.labUserName.Text = null;
+            this.labUserName.TextAlignment = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // guna2HtmlLabel1
+            // 
+            this.guna2HtmlLabel1.AutoSize = false;
+            this.guna2HtmlLabel1.BackColor = System.Drawing.Color.Transparent;
+            this.guna2HtmlLabel1.Font = new System.Drawing.Font("Andalus", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.guna2HtmlLabel1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.guna2HtmlLabel1.Location = new System.Drawing.Point(770, 38);
+            this.guna2HtmlLabel1.Name = "guna2HtmlLabel1";
+            this.guna2HtmlLabel1.Size = new System.Drawing.Size(51, 29);
+            this.guna2HtmlLabel1.TabIndex = 0;
+            this.guna2HtmlLabel1.Text = "User :";
+            // 
+            // btnFindClient
+            // 
+            this.btnFindClient.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.btnFindClient.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.btnFindClient.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btnFindClient.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.btnFindClient.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnFindClient.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(2)))), ((int)(((byte)(21)))), ((int)(((byte)(38)))));
+            this.btnFindClient.Font = new System.Drawing.Font("Andalus", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFindClient.ForeColor = System.Drawing.Color.White;
+            this.btnFindClient.Image = ((System.Drawing.Image)(resources.GetObject("btnFindClient.Image")));
+            this.btnFindClient.ImageAlign = System.Windows.Forms.HorizontalAlignment.Left;
+            this.btnFindClient.ImageSize = new System.Drawing.Size(40, 40);
+            this.btnFindClient.Location = new System.Drawing.Point(0, 322);
+            this.btnFindClient.Name = "btnFindClient";
+            this.btnFindClient.Size = new System.Drawing.Size(210, 51);
+            this.btnFindClient.TabIndex = 5;
+            this.btnFindClient.Tag = "Find Client";
+            this.btnFindClient.Text = "Users";
+            // 
+            // btnShowClients
+            // 
+            this.btnShowClients.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.btnShowClients.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.btnShowClients.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btnShowClients.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.btnShowClients.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnShowClients.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(2)))), ((int)(((byte)(21)))), ((int)(((byte)(38)))));
+            this.btnShowClients.Font = new System.Drawing.Font("Andalus", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnShowClients.ForeColor = System.Drawing.Color.White;
+            this.btnShowClients.Image = ((System.Drawing.Image)(resources.GetObject("btnShowClients.Image")));
+            this.btnShowClients.ImageAlign = System.Windows.Forms.HorizontalAlignment.Left;
+            this.btnShowClients.ImageSize = new System.Drawing.Size(40, 40);
+            this.btnShowClients.Location = new System.Drawing.Point(0, 271);
+            this.btnShowClients.Name = "btnShowClients";
+            this.btnShowClients.Size = new System.Drawing.Size(210, 51);
+            this.btnShowClients.TabIndex = 4;
+            this.btnShowClients.Text = "Customers";
+            // 
+            // btnAddNew
+            // 
+            this.btnAddNew.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.btnAddNew.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.btnAddNew.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btnAddNew.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.btnAddNew.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnAddNew.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(2)))), ((int)(((byte)(21)))), ((int)(((byte)(38)))));
+            this.btnAddNew.Font = new System.Drawing.Font("Andalus", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAddNew.ForeColor = System.Drawing.Color.White;
+            this.btnAddNew.Image = ((System.Drawing.Image)(resources.GetObject("btnAddNew.Image")));
+            this.btnAddNew.ImageAlign = System.Windows.Forms.HorizontalAlignment.Left;
+            this.btnAddNew.ImageSize = new System.Drawing.Size(40, 40);
+            this.btnAddNew.Location = new System.Drawing.Point(0, 169);
+            this.btnAddNew.Name = "btnAddNew";
+            this.btnAddNew.Size = new System.Drawing.Size(210, 51);
+            this.btnAddNew.TabIndex = 2;
+            this.btnAddNew.Tag = "";
+            this.btnAddNew.Text = "Books";
+            // 
+            // guna2Panel2
+            // 
+            this.guna2Panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(2)))), ((int)(((byte)(21)))), ((int)(((byte)(38)))));
+            this.guna2Panel2.Controls.Add(this.btnTitle);
+            this.guna2Panel2.Controls.Add(this.labUserName);
+            this.guna2Panel2.Controls.Add(this.guna2HtmlLabel1);
+            this.guna2Panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.guna2Panel2.Location = new System.Drawing.Point(210, 0);
+            this.guna2Panel2.Name = "guna2Panel2";
+            this.guna2Panel2.Size = new System.Drawing.Size(998, 84);
+            this.guna2Panel2.TabIndex = 4;
+            // 
+            // btnUpdateClient
+            // 
+            this.btnUpdateClient.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.btnUpdateClient.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.btnUpdateClient.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btnUpdateClient.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.btnUpdateClient.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnUpdateClient.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(2)))), ((int)(((byte)(21)))), ((int)(((byte)(38)))));
+            this.btnUpdateClient.Font = new System.Drawing.Font("Andalus", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUpdateClient.ForeColor = System.Drawing.Color.White;
+            this.btnUpdateClient.Image = ((System.Drawing.Image)(resources.GetObject("btnUpdateClient.Image")));
+            this.btnUpdateClient.ImageAlign = System.Windows.Forms.HorizontalAlignment.Left;
+            this.btnUpdateClient.ImageSize = new System.Drawing.Size(40, 40);
+            this.btnUpdateClient.Location = new System.Drawing.Point(0, 220);
+            this.btnUpdateClient.Name = "btnUpdateClient";
+            this.btnUpdateClient.Size = new System.Drawing.Size(210, 51);
+            this.btnUpdateClient.TabIndex = 3;
+            this.btnUpdateClient.Text = "   Authors";
+            // 
+            // guna2Panel1
+            // 
+            this.guna2Panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(2)))), ((int)(((byte)(21)))), ((int)(((byte)(38)))));
+            this.guna2Panel1.Controls.Add(this.guna2HtmlLabel5);
+            this.guna2Panel1.Controls.Add(this.guna2HtmlLabel3);
+            this.guna2Panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.guna2Panel1.Location = new System.Drawing.Point(0, 0);
+            this.guna2Panel1.Name = "guna2Panel1";
+            this.guna2Panel1.Size = new System.Drawing.Size(210, 169);
+            this.guna2Panel1.TabIndex = 0;
+            // 
+            // guna2HtmlLabel5
+            // 
+            this.guna2HtmlLabel5.AutoSize = false;
+            this.guna2HtmlLabel5.BackColor = System.Drawing.Color.Transparent;
+            this.guna2HtmlLabel5.Font = new System.Drawing.Font("Andalus", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.guna2HtmlLabel5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(112)))), ((int)(((byte)(66)))), ((int)(((byte)(100)))));
+            this.guna2HtmlLabel5.Location = new System.Drawing.Point(113, 115);
+            this.guna2HtmlLabel5.Name = "guna2HtmlLabel5";
+            this.guna2HtmlLabel5.Size = new System.Drawing.Size(90, 43);
+            this.guna2HtmlLabel5.TabIndex = 49;
+            this.guna2HtmlLabel5.Text = " System";
+            // 
+            // guna2HtmlLabel3
+            // 
+            this.guna2HtmlLabel3.AutoSize = false;
+            this.guna2HtmlLabel3.BackColor = System.Drawing.Color.Transparent;
+            this.guna2HtmlLabel3.Font = new System.Drawing.Font("Andalus", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.guna2HtmlLabel3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(173)))), ((int)(((byte)(73)))), ((int)(((byte)(225)))));
+            this.guna2HtmlLabel3.Location = new System.Drawing.Point(21, 115);
+            this.guna2HtmlLabel3.Name = "guna2HtmlLabel3";
+            this.guna2HtmlLabel3.Size = new System.Drawing.Size(155, 43);
+            this.guna2HtmlLabel3.TabIndex = 48;
+            this.guna2HtmlLabel3.Text = "Library";
+            // 
+            // pnlOptions
+            // 
+            this.pnlOptions.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(2)))), ((int)(((byte)(21)))), ((int)(((byte)(38)))));
+            this.pnlOptions.Controls.Add(this.btnLogout);
+            this.pnlOptions.Controls.Add(this.btnFindClient);
+            this.pnlOptions.Controls.Add(this.btnShowClients);
+            this.pnlOptions.Controls.Add(this.btnUpdateClient);
+            this.pnlOptions.Controls.Add(this.btnAddNew);
+            this.pnlOptions.Controls.Add(this.guna2Panel1);
+            this.pnlOptions.Dock = System.Windows.Forms.DockStyle.Left;
+            this.pnlOptions.Location = new System.Drawing.Point(0, 0);
+            this.pnlOptions.Name = "pnlOptions";
+            this.pnlOptions.Size = new System.Drawing.Size(210, 712);
+            this.pnlOptions.TabIndex = 3;
+            // 
+            // panelMainMenu
+            // 
+            this.panelMainMenu.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(2)))), ((int)(((byte)(21)))), ((int)(((byte)(38)))));
+            this.panelMainMenu.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelMainMenu.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(30)))), ((int)(((byte)(67)))));
+            this.panelMainMenu.Location = new System.Drawing.Point(0, 0);
+            this.panelMainMenu.Name = "panelMainMenu";
+            this.panelMainMenu.Size = new System.Drawing.Size(1208, 712);
+            this.panelMainMenu.TabIndex = 5;
+            this.panelMainMenu.Paint += new System.Windows.Forms.PaintEventHandler(this.panelMainMenu_Paint);
+            // 
+            // MainMenuScreen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1208, 712);
+            this.Controls.Add(this.guna2Panel2);
+            this.Controls.Add(this.pnlOptions);
+            this.Controls.Add(this.panelMainMenu);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "MainMenuScreen";
+            this.Text = "Form1";
+            this.guna2Panel2.ResumeLayout(false);
+            this.guna2Panel2.PerformLayout();
+            this.guna2Panel1.ResumeLayout(false);
+            this.pnlOptions.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private Guna.UI2.WinForms.Guna2Button btnTitle;
+        private Guna.UI2.WinForms.Guna2Button btnLogout;
+        private Guna.UI2.WinForms.Guna2HtmlLabel labUserName;
+        private Guna.UI2.WinForms.Guna2HtmlLabel guna2HtmlLabel1;
+        private Guna.UI2.WinForms.Guna2Button btnFindClient;
+        private Guna.UI2.WinForms.Guna2Button btnShowClients;
+        private Guna.UI2.WinForms.Guna2Button btnAddNew;
+        private Guna.UI2.WinForms.Guna2Panel guna2Panel2;
+        private Guna.UI2.WinForms.Guna2Button btnUpdateClient;
+        private Guna.UI2.WinForms.Guna2Panel guna2Panel1;
+        private Guna.UI2.WinForms.Guna2HtmlLabel guna2HtmlLabel5;
+        private Guna.UI2.WinForms.Guna2HtmlLabel guna2HtmlLabel3;
+        private Guna.UI2.WinForms.Guna2Panel pnlOptions;
+        private Guna.UI2.WinForms.Guna2Panel panelMainMenu;
+    }
+}
+
---
+++ LibrarySystemDesktopUI/MainMenuScreen.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows.Forms;
+
+namespace LibraryStstem
+{
+    public partial class MainMenuScreen : Form
+    {
+        public MainMenuScreen()
+        {
+            InitializeComponent();
+        }
+
+        private void panelMainMenu_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
