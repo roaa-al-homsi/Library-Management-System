1ec1eff6adc5655dc14e31898243eaeb903917d7
roaa-al-homsi <roaakokalhomsi@gmail.com>
2024-10-20 00:56:50 +0300

    ADD (Add|Update Person) form to Desktop UI

---
+++ LibrarySystemDesktopUI/Persons/AddOrUpdatePerson.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace LibraryStstem.Persons
+{
+    partial class frmAddPerson
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.uctrlPersonInfo1 = new LibraryStstem.UctrlPersonInfo();
+            this.guna2HtmlLabel1 = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            this.labPersoId = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            this.btnSave = new Guna.UI2.WinForms.Guna2Button();
+            this.btnCancel = new Guna.UI2.WinForms.Guna2Button();
+            this.SuspendLayout();
+            // 
+            // uctrlPersonInfo1
+            // 
+            this.uctrlPersonInfo1.BirthDate = new System.DateTime(2024, 10, 19, 22, 56, 7, 337);
+            this.uctrlPersonInfo1.ContactInfo = "";
+            this.uctrlPersonInfo1.Country = "";
+            this.uctrlPersonInfo1.FullName = "";
+            this.uctrlPersonInfo1.ImagePath = null;
+            this.uctrlPersonInfo1.Location = new System.Drawing.Point(69, 12);
+            this.uctrlPersonInfo1.Name = "uctrlPersonInfo1";
+            this.uctrlPersonInfo1.Size = new System.Drawing.Size(358, 431);
+            this.uctrlPersonInfo1.TabIndex = 0;
+            this.uctrlPersonInfo1.Load += new System.EventHandler(this.uctrlPersonInfo1_Load);
+            // 
+            // guna2HtmlLabel1
+            // 
+            this.guna2HtmlLabel1.AutoSize = false;
+            this.guna2HtmlLabel1.BackColor = System.Drawing.Color.Transparent;
+            this.guna2HtmlLabel1.Font = new System.Drawing.Font("Andalus", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.guna2HtmlLabel1.Location = new System.Drawing.Point(81, 449);
+            this.guna2HtmlLabel1.Name = "guna2HtmlLabel1";
+            this.guna2HtmlLabel1.Size = new System.Drawing.Size(95, 32);
+            this.guna2HtmlLabel1.TabIndex = 1;
+            this.guna2HtmlLabel1.Text = "Person ID:";
+            // 
+            // labPersoId
+            // 
+            this.labPersoId.AutoSize = false;
+            this.labPersoId.BackColor = System.Drawing.Color.Transparent;
+            this.labPersoId.Font = new System.Drawing.Font("Andalus", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labPersoId.Location = new System.Drawing.Point(261, 449);
+            this.labPersoId.Name = "labPersoId";
+            this.labPersoId.Size = new System.Drawing.Size(95, 32);
+            this.labPersoId.TabIndex = 2;
+            this.labPersoId.Text = "??";
+            this.labPersoId.TextAlignment = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Animated = true;
+            this.btnSave.AutoRoundedCorners = true;
+            this.btnSave.BorderRadius = 26;
+            this.btnSave.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.btnSave.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.btnSave.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btnSave.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.btnSave.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(29)))), ((int)(((byte)(44)))));
+            this.btnSave.Font = new System.Drawing.Font("Andalus", 12F, System.Drawing.FontStyle.Bold);
+            this.btnSave.ForeColor = System.Drawing.Color.White;
+            this.btnSave.Location = new System.Drawing.Point(114, 503);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(96, 54);
+            this.btnSave.TabIndex = 113;
+            this.btnSave.Text = "Save";
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Animated = true;
+            this.btnCancel.AutoRoundedCorners = true;
+            this.btnCancel.BorderRadius = 26;
+            this.btnCancel.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.btnCancel.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.btnCancel.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btnCancel.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.btnCancel.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(29)))), ((int)(((byte)(44)))));
+            this.btnCancel.Font = new System.Drawing.Font("Andalus", 12F, System.Drawing.FontStyle.Bold);
+            this.btnCancel.ForeColor = System.Drawing.Color.White;
+            this.btnCancel.Location = new System.Drawing.Point(242, 503);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(96, 54);
+            this.btnCancel.TabIndex = 114;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // frmAddPerson
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Silver;
+            this.ClientSize = new System.Drawing.Size(510, 558);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.labPersoId);
+            this.Controls.Add(this.guna2HtmlLabel1);
+            this.Controls.Add(this.uctrlPersonInfo1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "frmAddPerson";
+            this.Tag = "Add | Update Person";
+            this.Text = "Add | Update Person";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private UctrlPersonInfo uctrlPersonInfo1;
+        private Guna.UI2.WinForms.Guna2HtmlLabel guna2HtmlLabel1;
+        private Guna.UI2.WinForms.Guna2HtmlLabel labPersoId;
+        private Guna.UI2.WinForms.Guna2Button btnSave;
+        private Guna.UI2.WinForms.Guna2Button btnCancel;
+    }
+}
---
+++ LibrarySystemDesktopUI/Persons/AddOrUpdatePerson.cs
@@ -0,0 +1,72 @@
+﻿using LibrarySystemBusiness;
+using System;
+using System.Windows.Forms;
+namespace LibraryStstem.Persons
+{
+    public partial class frmAddPerson : Form
+    {
+        private enum Mode { Add, Update }
+        private Mode _Mode;
+        private int _PersonId;
+        private Person _Person;
+        public frmAddPerson(int Id)
+        {
+            InitializeComponent();
+            _PersonId = Id;
+            _Mode = (_PersonId == -1) ? Mode.Add : Mode.Update;
+        }
+        private void _LoadPersonData()
+        {
+            if (_Mode == Mode.Add)
+            {
+                _Person = new Person();
+                return;
+            }
+            _Person = Person.Find(_PersonId);
+            uctrlPersonInfo1.FullName = _Person.Name;
+            uctrlPersonInfo1.Country = _Person.Country;
+            uctrlPersonInfo1.ContactInfo = _Person.ContactInfo;
+            uctrlPersonInfo1.BirthDate = _Person.BirthDate;
+            //image
+        }
+
+        private void uctrlPersonInfo1_Load(object sender, System.EventArgs e)
+        {
+
+        }
+        private void _FillPersonBeforeSave()
+        {
+            _Person.Name = uctrlPersonInfo1.FullName;
+            _Person.ContactInfo = uctrlPersonInfo1.ContactInfo;
+            _Person.BirthDate = uctrlPersonInfo1.BirthDate;
+            _Person.Country = uctrlPersonInfo1.Country;
+            _Person.ImagePath = (!string.IsNullOrWhiteSpace(uctrlPersonInfo1.ImagePath)) ? uctrlPersonInfo1.ImagePath : null;
+
+        }
+        private void btnSave_Click(object sender, System.EventArgs e)
+        {
+            _FillPersonBeforeSave();
+            if (_Person.Save())
+            {
+                MessageBox.Show("Data Saved Successfully", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("Failed Successfully", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        private void _BackDefaultForm()
+        {
+            uctrlPersonInfo1.FullName = "???";
+            uctrlPersonInfo1.Country = "???";
+            uctrlPersonInfo1.ContactInfo = "???";
+            uctrlPersonInfo1.BirthDate = new DateTime(1999, 1, 1);
+            labPersoId.Text = "???";
+            uctrlPersonInfo1.ImagePath = null;
+        }
+        private void btnCancel_Click(object sender, System.EventArgs e)
+        {
+            _BackDefaultForm();
+        }
+    }
+}
