d0c3aeef2c32c2e0653ff9f9d62977f51ecc7178
roaa-al-homsi <roaakokalhomsi@gmail.com>
2024-10-20 20:36:37 +0300

    ADD Genre class to data access and business layers with these methods (Add,Update,All,Exist)

---
+++ LibrarySystemBusiness/Genre.cs
@@ -0,0 +1,39 @@
+﻿using LibrarySystemDataAccess;
+
+namespace LibrarySystemBusiness
+{
+    public class Genre
+    {
+        private enum Mode { Add, Update };
+        private Mode _Mode;
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public Genre()
+        {
+            this.Id = -1;
+            this.Name = string.Empty;
+            _Mode = Mode.Add;
+        }
+        private bool _Add()
+        {//validation
+            this.Id = GenreData.Add(this.Id, this.Name);
+            return (this.Id != -1);
+        }
+        private bool _Update()
+        {//validation
+            return GenreData.Update(this.Id, this.Name);
+        }
+        public bool Save()
+        {
+            switch (_Mode)
+            {
+                case Mode.Add:
+                    return _Add();
+                case Mode.Update:
+                    return _Update();
+                default: return false;
+            }
+        }
+    }
+}
---
+++ LibrarySystemDataAccess/GenreData.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace LibrarySystemDataAccess
+{
+    static public class GenreData
+    {
+        static public int Add(int Id, string Name)
+        {
+            int NewIdRecord = 0;
+            SqlConnection connection = new SqlConnection(SettingData.ConnectionString);
+            string query = @"insert into Genres (Id,Name)values (@Id,@Name)
+                           SELECT SCOPE_IDENTITY();";
+            SqlCommand command = new SqlCommand(query, connection);
+            command.Parameters.AddWithValue("@Id", Id);
+            command.Parameters.AddWithValue("@Name", Name);
+            try
+            {
+                connection.Open();
+                object Result = command.ExecuteScalar();
+                if (Result != null && int.TryParse(Result.ToString(), out int insertedId))
+                {
+                    NewIdRecord = insertedId;
+                }
+            }
+            catch { }
+            finally { connection.Close(); }
+            return NewIdRecord;
+        }
+        static public bool Update(int Id, string Name)
+        {
+            int RowAffected = 0;
+            SqlConnection connection = new SqlConnection(SettingData.ConnectionString);
+            string query = @"update Genres set Id=@Id ,Name=@Name where Id=@Id";
+
+            SqlCommand command = new SqlCommand(query, connection);
+            command.Parameters.AddWithValue("@Id", Id);
+            command.Parameters.AddWithValue("@Name", Name);
+            try
+            {
+                connection.Open();
+                RowAffected = command.ExecuteNonQuery();
+            }
+            catch { Exception exception; }
+            finally { connection.Close(); }
+            return RowAffected > 0;
+        }
+        static public bool Delete(int Id)
+        {
+            return GenericData.Delete("Delete Genres where Id=@Id", "@Id", Id);
+        }
+        static public DataTable All()
+        {
+            return GenericData.All(" select *from Genres");
+        }
+        static public bool Exist(int Id)
+        {
+            return GenericData.Exist("select Found=1 from Genres where Id =@Id", "@Id", Id);
+        }
+
+    }
+}
